829ac354c5282845e946c6327f47dc8cd6bafb36
Martin Meyer <martin.meyer@inftec.ch>
2016-05-04 16:12:05 +0200

    PR-285 Added ju-ee-ear

---
+++ ju-ee-ear-jar/src/test/java/ch/inftec/webapp/ear/test/SimpeJarIT.java
@@ -0,0 +1,10 @@
+package ch.inftec.webapp.ear.test;
+
+import org.junit.Test;
+
+public class SimpeJarIT {
+	@Test
+	public void simpleJarTest() {
+		System.out.println("Simple JAR Integration Test");
+	}
+}
---
+++ ju-ee-ear-jar/src/test/java/ch/inftec/webapp/ear/test/SimpleJarTest.java
@@ -0,0 +1,10 @@
+package ch.inftec.webapp.ear.test;
+
+import org.junit.Test;
+
+public class SimpleJarTest {
+	@Test
+	public void simpleJarTest() {
+		System.out.println("Simple JAR Unit Test");
+	}
+}
