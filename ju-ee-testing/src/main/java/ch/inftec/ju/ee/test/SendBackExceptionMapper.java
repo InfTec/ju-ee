1986a9490c3354e3dfaecf240f7b2f4f1bae3575
Stefan Andonie <stefan.andonie@inftec.ch>
2017-01-05 15:28:10 +0100

    ESWM-286 class for intercepting and Sending Back Exceptions

---
+++ ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/SendBackExceptionMapper.java
@@ -0,0 +1,34 @@
+package ch.inftec.ju.ee.test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.logging.Logger;
+
+@Provider
+public class SendBackExceptionMapper implements ExceptionMapper<Exception>{
+
+	Logger logger = Logger.getLogger(SendBackExceptionMapper.class);
+	
+	@Override
+	public Response toResponse(Exception exception) {
+		logger.info("Exception Intercepted");
+		logger.info(convertStacktraceToString(exception));
+		return Response.ok(exception,MediaType.APPLICATION_JSON).status(Response.Status.INTERNAL_SERVER_ERROR).build();
+	}
+	
+	private String convertStacktraceToString(Exception exception){
+        StringWriter stringWritter = new StringWriter();
+        PrintWriter printWritter = new PrintWriter(stringWritter, true);
+        exception.printStackTrace(printWritter);
+        printWritter.flush();
+        stringWritter.flush(); 
+        return stringWritter.toString();
+	}
+
+}
