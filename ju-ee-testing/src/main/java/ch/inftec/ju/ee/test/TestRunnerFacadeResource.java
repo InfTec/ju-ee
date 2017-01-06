5adfde5d33cb42851584adaf88f6f11d435cd39e
Stefan Andonie <stefan.andonie@inftec.ch>
2017-01-06 12:13:52 +0100

    ESWM-286 added Annotations to make it a stateless Bean + removed most of the test logging / now able to implement it loke the TestRunner Facade Bean, json Handling etc moved to Background

--- ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/TestRunnerFacadeResource.java
+++ ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/TestRunnerFacadeResource.java
@@ -1,8 +1,10 @@
 package ch.inftec.ju.ee.test;
 
+import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.TransactionScoped;
 import javax.transaction.UserTransaction;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -21,12 +23,17 @@ import ch.inftec.ju.db.TxHandler;
 import ch.inftec.ju.ee.test.TestRunnerAnnotationHandler.ContainerTestContextSetter;
 import ch.inftec.ju.util.SystemPropertyTempSetter;
 import javax.ejb.TransactionManagementType;
+import javax.enterprise.context.RequestScoped;
 
+
+//@Path("/testRunner")
+@Stateless
 @TransactionManagement(TransactionManagementType.BEAN)
-@Path("/testRunner")
 public class TestRunnerFacadeResource implements TestRunnerFacade {
 
 	private Logger logger = Logger.getLogger(TestRunnerFacadeResource.class);
+	
+	
 	
 	@Inject
 	private UserTransaction tx;
@@ -49,37 +56,41 @@ public class TestRunnerFacadeResource implements TestRunnerFacade {
 	@Override
 	public SystemPropertyTempSetter runPreTestActionsInEjbContext(TestRunnerAnnotationHandler handler) throws Exception {
 		
+		/*
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setVisibility(PropertyAccessor.FIELD,Visibility.ANY);
 		String jsonString = mapper.writeValueAsString(handler);
 		
 		logger.info(jsonString);
+		*/
+		
 		logger.info("execute Pre run Actions");
-		logger.info(handler);
 
 		try (ContainerTestContextSetter s = handler.new ContainerTestContextSetter()) {
 
 			SystemPropertyTempSetter tempSetter = handler.initContainerTestEnv();
 
 			try (TxHandler txHandler = new TxHandler(this.tx, true)) {
-				
-				
-				logger.info("this.em is null : " + (this.em == null));
-				
-				logger.info("step 0");
 				// Execute post test annotations (dataset exporting, data verifying)
 				handler.executePreTestAnnotations(new JuEmUtil(this.em));
-				logger.info("step 1");
-				
 				txHandler.commit(); // Commit after data verifying / exporting
-				logger.info("step 2");
-
 			} catch (Exception ex) {
 				// Reset properties in case of an exception
 				tempSetter.close();
 				throw ex;
 			}
 
+			/* Test Map Serialization
+			
+			tempSetter.setProperty("foo", "bar");
+			tempSetter.setProperty("21", "27");
+			tempSetter.setProperty("key", "value");
+			
+			tempSetter.setEnv("aaaaaa", "bbbbbb");
+			tempSetter.setEnv("env", "env");
+			tempSetter.setEnv("44", "55");
+
+			*/
 			return tempSetter;
 		}
 	}
