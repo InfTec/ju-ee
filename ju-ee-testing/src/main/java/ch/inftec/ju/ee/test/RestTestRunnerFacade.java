947405702cf1cd67a3e58ec71036994af200702a
Stefan Andonie <stefan.andonie@inftec.ch>
2017-01-06 16:09:30 +0100

    ESWM-286 implemented the rest of methods call the coresponding rest resources + put response handling in a seperate method + exception handling: expecting a string of stacktrace or error message

--- ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/RestTestRunnerFacade.java
+++ ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/RestTestRunnerFacade.java
@@ -2,10 +2,8 @@ package ch.inftec.ju.ee.test;
 
 import ch.inftec.ju.util.SystemPropertyTempSetter;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -56,89 +54,138 @@ public class RestTestRunnerFacade implements TestRunnerFacade{
 	@Override
 	public SystemPropertyTempSetter runPreTestActionsInEjbContext(TestRunnerAnnotationHandler handler) throws Exception {
 		
-		try{
-
 		String jsonString = objectMapper.writeValueAsString(handler);		
-		logger.info(jsonString);
-
 		Client client = ClientBuilder.newClient();
 		//ResteasyClient client = new ResteasyClientBuilder().build();
 		WebTarget SimpleResource = client.target(RESOURCE_APP_PATH + "preTestActions");
-		
 		Invocation preTestInvocation = SimpleResource.request(MediaType.APPLICATION_JSON).buildPost(Entity.entity(jsonString, MediaType.APPLICATION_JSON));
-		
 		logger.info("[preTestActions] invoke Rest request");
 		Response response = preTestInvocation.invoke();
+		SystemPropertyTempSetter tempSetter = handleResponse(response, SystemPropertyTempSetter.class);
+		return tempSetter;
+	}
 
+	@Override
+	public void runTestMethodInEjbContext(TestRunnerAnnotationHandler handler) throws Exception {
+		
+		String jsonString = objectMapper.writeValueAsString(handler);
+		Client client = ClientBuilder.newClient();
+		WebTarget SimpleResource = client.target(RESOURCE_APP_PATH + "runMethodinEjb");
+		Invocation preTestInvocation = SimpleResource.request(MediaType.APPLICATION_JSON).buildPost(Entity.entity(jsonString, MediaType.APPLICATION_JSON));
+		logger.info("[runTestMethodInEjbContext] invoke Rest request");
+		Response response = preTestInvocation.invoke();
+		handleResponse(response);
+	}
+
+	@Override
+	public void runPostTestActionsInEjbContext(TestRunnerAnnotationHandler handler) throws Exception {
+		String jsonString = objectMapper.writeValueAsString(handler);
+		Client client = ClientBuilder.newClient();
+		WebTarget SimpleResource = client.target(RESOURCE_APP_PATH + "postTestActions");
+		Invocation preTestInvocation = SimpleResource.request(MediaType.APPLICATION_JSON).buildPost(Entity.entity(jsonString, MediaType.APPLICATION_JSON));
+		logger.info("[runPostTestActionsInEjbContext] invoke Rest request");
+		Response response = preTestInvocation.invoke();
+		handleResponse(response);		
+	}
+
+	@Override
+	public void cleanupTestRun(TestRunnerAnnotationHandler handler, SystemPropertyTempSetter tempSetter) {
+		
+		try{
+		CleanupRestParamEncapsulationObject params = new CleanupRestParamEncapsulationObject();
+		params.handler = handler;
+		params.tempSetter = tempSetter;
+		String jsonString = objectMapper.writeValueAsString(params);
+		logger.info(jsonString);
+		Client client = ClientBuilder.newClient();
+		WebTarget SimpleResource = client.target(RESOURCE_APP_PATH + "cleanupTestRun");
+		Invocation preTestInvocation = SimpleResource.request(MediaType.APPLICATION_JSON).buildPost(Entity.entity(jsonString, MediaType.APPLICATION_JSON));
+		logger.info("[cleanupTestRun] invoke Rest request");
+		Response response = preTestInvocation.invoke();
+		handleResponse(response);
+		
+		}catch(Exception exception){
+			logger.error(exception);
+		}
+	}
+
+	@Override
+	public Object runMethodInEjbContext(String className, String methodName, Class<?>[] parameterTypes, Object[] args) throws Exception {
+
+		RunMethodRestParamEncapsulationObject params = new RunMethodRestParamEncapsulationObject();
+		params.className = className;
+		params.methodName = methodName;
+		params.parameterTypes = parameterTypes;
+		params.args = args;
+		String jsonString = objectMapper.writeValueAsString(params);
+		Client client = ClientBuilder.newClient();
+		WebTarget SimpleResource = client.target(RESOURCE_APP_PATH + "runMethod2");
+		Invocation preTestInvocation = SimpleResource.request(MediaType.APPLICATION_JSON).buildPost(Entity.entity(jsonString, MediaType.APPLICATION_JSON));
+		logger.info("[runPostTestActionsInEjbContext] invoke Rest request");
+		Response response = preTestInvocation.invoke();
+		return handleResponse(response,Object.class);
+	}
+	
+	private void handleResponse(Response response) throws Exception {
+		
+		int status = response.getStatus();
+		if(status < 300 ){
+			logger.info("Response OK : " + status);
+		}else{
+			getExceptionFromResponse(response);
+		}
+	}
+	
+	private <T> T handleResponse(Response response, Class<T> objClass) throws Exception {
+		
 		int status = response.getStatus();
 		
 		// Recover Return value from Response
-		if(status < 300){
-				logger.info("[preTestActions] OK Response :" + status);
+		if(status < 300 ){
+				logger.info("Response OK : " + status);
 				
-				jsonString = response.readEntity(String.class);
+				String jsonString = response.readEntity(String.class);
 				logger.info(jsonString);
-				SystemPropertyTempSetter tempsetter = objectMapper.readValue(jsonString, SystemPropertyTempSetter.class);
-				return tempsetter;
-		}else{
-			logger.info("[preTestActions] Response Code : " + status);
+				T obj = objectMapper.readValue(jsonString, objClass);
+				return obj;
 		}
 		
+		getExceptionFromResponse(response);
+		
+		return null;
+	}
+	
+	private void getExceptionFromResponse(Response response) throws Exception{
+				
 		// Try to find reason for error
+		//if(status >= 400 && status < 500) logger.info("[preTestActions] Service not found");
 		
-		if(status >= 400 && status < 500){
-				logger.info("[preTestActions] Service not found");
-		}	
-		
-		
+		int status = response.getStatus();
+		logger.info("Response Code : " + status);
+
 		// reconstruct the exception
 		if(status >= 500){
-			logger.info("[preTestActions] Internal Server error");
+			logger.info("Internal Server error");
+			
 			Object ex = null;
 			try{
-				ex =  response.readEntity(Exception.class);
+				//ex =  response.readEntity(Exception.class);
+				
+				logger.error(response.readEntity(String.class));
 			}catch(Exception exception){
 				logger.error("Unable to deserialize Response body");
 				throw exception;
 			}
+			
+			/*
 			if(ex instanceof String){
-				logger.error(ex);
+				logger.error((String) ex);
 				throw new InternalServerErrorException((String) ex);
 			}
 			if(ex instanceof Exception ){
 				throw (Exception) ex;
 			}
+			*/
 		}
-
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		return null;
 	}
-
-	@Override
-	public void runTestMethodInEjbContext(TestRunnerAnnotationHandler handler) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void runPostTestActionsInEjbContext(TestRunnerAnnotationHandler handler) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void cleanupTestRun(TestRunnerAnnotationHandler handler, SystemPropertyTempSetter tempSetter) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Object runMethodInEjbContext(String className, String methodName, Class<?>[] parameterTypes, Object[] args) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
