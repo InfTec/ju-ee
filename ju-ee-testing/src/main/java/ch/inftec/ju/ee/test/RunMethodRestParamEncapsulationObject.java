5b0cc69c154f6985238462f604b1d0cf647c3ee5
Stefan Andonie <stefan.andonie@inftec.ch>
2017-01-06 16:12:43 +0100

    ESWM-286 simple Object to encapsulate parameters of TestRunnerFacade.runMethodInEjbContext in order to send only one json object in request body

---
+++ ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/RunMethodRestParamEncapsulationObject.java
@@ -0,0 +1,12 @@
+package ch.inftec.ju.ee.test;
+
+import ch.inftec.ju.util.SystemPropertyTempSetter;
+
+public class RunMethodRestParamEncapsulationObject {
+	
+	public String className, methodName;
+	public Class<?>[] parameterTypes;
+	Object[] args;
+	
+	public RunMethodRestParamEncapsulationObject(){}	
+}
