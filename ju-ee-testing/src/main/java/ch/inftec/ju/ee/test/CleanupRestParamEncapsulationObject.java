dffa0097d2821735c0d2fa025654f729fd47fdc4
Stefan Andonie <stefan.andonie@inftec.ch>
2017-01-06 16:14:30 +0100

    ESWM-286 simple Object to encapsulate parameters of TestRunnerFacade.cleanupTestRun in order to send one json object in requeuest body

---
+++ ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/CleanupRestParamEncapsulationObject.java
@@ -0,0 +1,11 @@
+package ch.inftec.ju.ee.test;
+
+import ch.inftec.ju.util.SystemPropertyTempSetter;
+
+public class CleanupRestParamEncapsulationObject {
+	
+	public TestRunnerAnnotationHandler handler;
+	public SystemPropertyTempSetter tempSetter;
+	
+	public CleanupRestParamEncapsulationObject(){}	
+}
