1b231436c1049c95a4d2cd441ba086727841361c
Stefan Andonie <stefan.andonie@inftec.ch>
2017-01-12 09:28:25 +0100

    ESWM-286 /ContainerTestContextResolver changed modules that are registered in the Objectmapper / SystemPropertyTempsetterSerializer

--- ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/provider/ContainerTestContextResolver.java
+++ ju-ee-testing/src/main/java/ch/inftec/ju/ee/test/provider/ContainerTestContextResolver.java
@@ -1,8 +1,9 @@
-package ch.inftec.ju.ee.test;
+package ch.inftec.ju.ee.test.provider;
 
 import java.io.IOException;
 import java.util.Map;
 
+import javax.annotation.Priority;
 import javax.ws.rs.Produces;
 import javax.ws.rs.ext.Provider;
 
@@ -13,6 +14,10 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
+import ch.inftec.ju.ee.test.TestRunnerAnnotationHandler;
+import ch.inftec.ju.ee.test.serialize.SystemPropertyTempSetterDeserializer;
+import ch.inftec.ju.ee.test.serialize.SystemPropertyTempSetterSerializer;
+import ch.inftec.ju.ee.test.serialize.TestRunnerAnnotationHandlerDeserializer;
 import ch.inftec.ju.util.SystemPropertyTempSetter;
 
 import javax.ws.rs.core.MediaType;
@@ -20,30 +25,29 @@ import javax.ws.rs.ext.ContextResolver;
 
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
-public class JacksonObjectMapperProvider implements ContextResolver<ObjectMapper>{
+public class ContainerTestContextResolver implements ContextResolver<ObjectMapper>{
 	
-	Logger logger = Logger.getLogger(JacksonObjectMapperProvider.class);
+	Logger logger = Logger.getLogger(ContainerTestContextResolver.class);
 	
 	private ObjectMapper objectMapper;
 	
-    public JacksonObjectMapperProvider() throws Exception {
+    public ContainerTestContextResolver() throws Exception {
         this.objectMapper = new ObjectMapper();
 		objectMapper.setVisibility(PropertyAccessor.FIELD,Visibility.ANY);
 	
-		// add custom modules Serializers and Deserializers
+		// add custom modules for Serializers and Deserializers
 		SimpleModule module = new SimpleModule();
-		module.addSerializer((Class<Map<String,String>>)(Class)Map.class, new MapSerializer());
+		module.addSerializer(SystemPropertyTempSetter.class, new SystemPropertyTempSetterSerializer());
 		objectMapper.registerModule(module);
 		
 		module = new SimpleModule();
-		module.addDeserializer((Class<Map<String,String>>)(Class)Map.class, new MapDeserializer());
+		module.addDeserializer(SystemPropertyTempSetter.class, new SystemPropertyTempSetterDeserializer());
 		objectMapper.registerModule(module);
 		
 		module = new SimpleModule();
 		module.addDeserializer(TestRunnerAnnotationHandler.class, new TestRunnerAnnotationHandlerDeserializer());
 		objectMapper.registerModule(module);
     }
-    
 
     public ObjectMapper getContext(Class<?> objectType) {
     	
